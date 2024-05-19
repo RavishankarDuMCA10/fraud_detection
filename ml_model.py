3ff9344d90e85d23becdd89f559b0bb1642a0833
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2024-05-19 22:40:15 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Adding all ml model related information")
+
+def model1():
+    print("adding code for Model1")
+
+def model2():
+    print("adding code for Model2")
+
+def model3():
+    print("adding code for Model3")
